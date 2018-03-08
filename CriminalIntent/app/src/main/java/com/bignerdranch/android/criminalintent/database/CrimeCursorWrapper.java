a75f87aa11616545fce00a103557219c8d4149eb
Tyler Radabaugh <radabaughtyler24@gmail.com>
2018-03-08 14:02:48 -0800

    Adding getCrime() method

--- CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/database/CrimeCursorWrapper.java
+++ CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/database/CrimeCursorWrapper.java
@@ -3,6 +3,9 @@ package com.bignerdranch.android.criminalintent.database;
 import android.database.Cursor;
 import android.database.CursorWrapper;
 
+import com.bignerdranch.android.criminalintent.Crime;
+import com.bignerdranch.android.criminalintent.database.CrimeDbSchema.CrimeTable;
+
 /**
  * Created by Tyler Radabaugh on 3/8/2018.
  */
@@ -10,5 +13,14 @@ import android.database.CursorWrapper;
 public class CrimeCursorWrapper extends CursorWrapper {
     public CrimeCursorWrapper(Cursor cursor) {
         super(cursor);
+    }
+
+    public Crime getCrime() {
+        String uuidString = getString(getColumnIndex(CrimeTable.Cols.UUID));
+        String title = getString(getColumnIndex(CrimeTable.Cols.TITLE));
+        long date = getLong(getColumnIndex(CrimeTable.Cols.DATE));
+        int isSolved = getInt(getColumnIndex(CrimeTable.Cols.SOLVED));
+
+        return null;
     }
 }
